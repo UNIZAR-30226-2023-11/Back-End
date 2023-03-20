b4dc1b1bbb8131e7916c3aba918c68d94d353c1f
795809 <795809@unizar.es>
2023-03-20 11:09:44 +0100

    Funciones findUser (LogIn) y createUser(Registrarse)

--- app_server/controllers/usersController.js
+++ app_server/controllers/usersController.js
@@ -8,18 +8,20 @@ async function createUser(req, res, next){
     req.params
 
     const doc = new modeloUser({
-        nombre: req.params.nombre,
-        telefono: req.params.telefono
-        // nombre: req.body.nombre,
+        //nombre: req.params.nombre,
+        //telefono: req.params.telefono
+        nombre: req.body.nombre,
+        telefono: req.body.telefono
         // telefono: req.body.telefono
     });
 
     try {
         await mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("Connected to MongoDB Atlas")
+        console.log("Connected to MongoDB Atlas");
 
         await doc.save();
-        console.log('Documento guardado correctamente')
+        console.log('Documento guardado correctamente');
+        res.status(201).send('Usuario creado correctamente');
         // res.status(201).json({message: 'Usuario creado correctamente'})
     }
     catch (error) {
@@ -32,4 +34,41 @@ async function deleteUser(req, res, next){
     console.log("***POST METHOD Eliminación de Usuario");
 }
 
-module.exports = {createUser, deleteUser};
+async function findUser(req, res, next){
+    console.log("***GET METHOD Creacion de Usuario");
+
+    req.params
+
+    // const doc = new modeloUser({
+    //     //nombre: req.params.nombre,
+    //     //telefono: req.params.telefono
+    //     nombre: req.body.nombre,
+    //     telefono: req.body.telefono
+    //     // telefono: req.body.telefono
+    // });
+
+    const doc = {nombre: req.body.nombre, telefono: req.body.telefono};
+
+    //await mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+
+    try {
+        await mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to MongoDB Atlas");
+      
+        const docs = await modeloUser.find(doc);
+      
+        if (docs.length > 0) {
+          console.log("Documento encontrado: ", docs);
+          res.status(200).send('El usuario ha iniciado sesión correctamente');
+        } else {
+          console.log('No se encontró el documento');
+          res.status(500).json({ error: 'Error el usuario no esta creado' });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error el usuario no esta creado' });
+      }
+      
+}
+module.exports = {createUser, deleteUser, findUser};
--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var ctrlUsers = require('../controllers/usersController');
 
-router.get('/:nombre/:telefono', ctrlUsers.createUser);
-router.get('/', ctrlUsers.deleteUser);
+//router.get('/:nombre/:telefono', ctrlUsers.createUser);
+router.get('/', ctrlUsers.findUser);
+router.post('/', ctrlUsers.createUser);
+router.delete('/', ctrlUsers.deleteUser);
 
 module.exports = router;
