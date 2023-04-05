326c35fa12e43c92eed7a3d16bb6ef1ced5d4843
DinoDani30 <danicarrizo30@gmail.com>
2023-04-05 16:28:42 +0200

    Cambios partida

--- app_server/routes/partida.js
+++ app_server/routes/partida.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var ctrlPartida = require('../controllers/partidaController');
 
 router.post('/crear', ctrlPartida.crearPartida);
+router.get('/encontrar', ctrlPartida.findPartida);
 
 module.exports = router;
