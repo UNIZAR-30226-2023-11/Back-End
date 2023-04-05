f9451506cd219426468f433deac59e84d8f1977e
795809 <795809@unizar.es>
2023-04-05 10:31:02 +0200

    Cambio de get a post de Login

--- app_server/routes/users.js
+++ app_server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var ctrlUsers = require('../controllers/usersController');
 
 router.post('/register', ctrlUsers.registerUser);
-router.get('/login', ctrlUsers.loginUser);
+router.post('/login', ctrlUsers.loginUser);
 router.delete('/delete', ctrlUsers.deleteUser);
 router.put('/updatePassword', ctrlUsers.updatePassword);
 router.put('/updateCorreo', ctrlUsers.updateCorreo);
