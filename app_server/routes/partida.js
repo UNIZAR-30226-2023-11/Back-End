b492f4580802f641b7d0547e1625ff1f00f0fdbb
DinoDani30 <danicarrizo30@gmail.com>
2023-04-05 17:10:05 +0200

    Cambios asignaturas y partida.js

--- app_server/routes/partida.js
+++ app_server/routes/partida.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 var ctrlPartida = require('../controllers/partidaController');
 
+var ctrlAsignatura = require('../controllers/asignaturasController');
+
+
 router.post('/crear', ctrlPartida.crearPartida);
-router.get('/encontrar', ctrlPartida.findPartida);
+router.post('/jugadoresUpdate', ctrlAsignatura.checkCasilla);
+//router.get('/encontrar', ctrlPartida.findPartida);  //Esta es privada
 
 module.exports = router;
