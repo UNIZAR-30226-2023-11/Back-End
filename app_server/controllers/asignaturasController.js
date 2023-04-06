b2de8f8cc987639c01f7d1b7aeecd62ce0741edf
795809 <795809@unizar.es>
2023-04-06 14:01:15 +0200

    funcion para chequear si una casilla esta comprada

--- app_server/controllers/asignaturasController.js
+++ app_server/controllers/asignaturasController.js
@@ -196,7 +196,7 @@ async function estaComprada(coordenadas,res){
 async function operativaCasilla(req, res){
     if(estaComprada(req.body.coordenadas, res)){
         console.log("El jugador", req.body.username, "esta en la casilla comprada tiene que pagar");
-        console.log(res.casillaComprada);
+        console.log(res);
         //Si la casilla esta comprada habrá que quitarle dinero al jugador y añadirselo al propietario
     }else{
         console.log("El jugador", req.body.username, "no tiene que pagar");
