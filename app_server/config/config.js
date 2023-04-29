e9fb227103ac236273d5845bc83e84e93abde6a0
DinoDani30 <danicarrizo30@gmail.com>
2023-04-29 13:31:04 +0200

    Cambios prueba docker mongo

--- app_server/config/config.js
+++ app_server/config/config.js
@@ -3,7 +3,7 @@ const config = {
     database: "Monopoly",
     colUsers: "users",
     //uri: "mongodb+srv://usuario:REuosfhhCZY0IvN3@ps-monopoly.uu23g8z.mongodb.net/Monopoly"
-    uri : "mongodb://usuario:REuosfhhCZY0IvN3@172.22.0.3:27017/Monopoly"
+    uri : "mongodb://admin:admin@172.22.0.3:27017/Monopoly"
   },
 };
 module.exports = config;
