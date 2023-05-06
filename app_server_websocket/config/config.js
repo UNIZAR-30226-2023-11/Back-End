9e8b9c39b4253942694d5b024cd781d85eb32829
lunaalvareez <815976@unizar.es>
2023-05-06 20:20:54 +0200

    mini cambio

--- app_server_websocket/config/config.js
+++ app_server_websocket/config/config.js
@@ -2,7 +2,7 @@ const config = {
   db: {
     database: "Monopoly",
     colUsers: "users",
-    uri: "mongodb+srv://usuario:REuosfhhCZY0IvN3@ps-monopoly.uu23g8z.mongodb.net/Monopoly"
+    uri: "mongodb+srv://usuario:REuosfhhCZY0IvN3@ps-monopoly.uu23g8z.mongodb.net/Monopoly",
     //uri : "mongodb://admin:admin@172.22.0.2:27017/Monopoly"
     dbOptions: {
       useNewUrlParser: true,
