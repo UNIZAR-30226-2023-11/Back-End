c50418b05932f5229655c5aa72fd138075c18bdb
DinoDani30 <danicarrizo30@gmail.com>
2023-05-08 17:07:22 +0200

    Changes usersController

--- app_server_websocket/controllers/usersController.js
+++ app_server_websocket/controllers/usersController.js
@@ -234,7 +234,7 @@ async function updateUsername(username, newusername) {
 
 async function devolverUsuario(username) {
     w.logger.info("***POST METHOD Devolver usuario");
-    console.log(username)
+    w.logger.verbose(username)
     const doc = {
         nombreUser: username
     };
@@ -249,7 +249,7 @@ async function devolverUsuario(username) {
             w.logger.debug("Documento encontrado: " + docs[0].toString);
             w.logger.verbose(docs[0]);
             const image = await modeloImagen.findOne({nombre: docs[0].imagen}).exec();
-            docs[0].imagen = image;
+            docs[0].imagen = imagen.image;
             return docs[0];
 
         } else {
