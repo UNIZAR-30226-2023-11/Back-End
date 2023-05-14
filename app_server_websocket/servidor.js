cf3c0f0efa55946184909731860b69dce7ddc72d
DinoDani30 <danicarrizo30@gmail.com>
2023-05-14 12:44:47 +0200

    servidor

--- app_server_websocket/servidor.js
+++ app_server_websocket/servidor.js
@@ -588,7 +588,7 @@ io.on('connection', (socket) => {
     var s = clientes[socketId].partidaActiva;
     io.to(s.toString()).emit('infoPartida', partida);
     if(partida.nombreJugadores.length <= 1){
-        
+      io.to(s.toString()).emit('finPartida', partida.nombreJugadores[0]);
     }
     var msg = "";
     var m = {
