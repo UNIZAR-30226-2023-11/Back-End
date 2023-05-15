958212200f477296b837b36f80620cd61b31e01e
Martina <795809@unizar.es>
2023-05-15 05:37:28 +0200

    Update cartasController.js

--- app_server_websocket/controllers/cartasController.js
+++ app_server_websocket/controllers/cartasController.js
@@ -317,7 +317,7 @@ async function tarjetaAleatoria(tipo, username, idPartida, coordenadas) {
         //     { $sample: { size: 1 } }
         // ]).exec();
 
-        const resultado = await modeloTarjetas.findOne({ nombre: "Vaya, tienes que ir a Segunda Convocatoria"}).exec();
+        const resultado = await modeloTarjetas.findOne({ descripcion: "La universidad te va a devolver dinero por un error en los cobros. Recibe 1€."}).exec();
 
         // idPartida = 1; resultado[0]
         await accionCarta(idPartida, username, resultado, coordenadas);
